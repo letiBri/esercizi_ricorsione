c2529370347eceb3e00cca9ffc56f803a7830da8
letiBri <s307875@studenti.polito.it>
2025-04-16 22:12:45 +0200

    lezione 16 aprile 2025

--- quadratoMagico.py
+++ quadratoMagico.py
@@ -1,22 +1,21 @@
 def quadrato_magico(N):  # N è la dimensione del quadrato magico
-    rimanenti = []
-    for i in range(N * N):
-        rimanenti.append(i)
+    rimanenti = list(range(1, N*N + 1))
     ricorsione([], rimanenti, N)
 
 def ricorsione(parziale, rimanenti, N):
     # condizione terminale
-    if len(parziale) == N*N: # mi fermo quando ho esaminato tutte le celle del quadrato
+    if len(parziale) == N*N:  # mi fermo quando ho esaminato tutte le celle del quadrato
         print(parziale)
     # caso ricorsivo
     else:
         for i in range(len(rimanenti)):
-            # mancaa il fatto di inserire i vincoli
+            # manca il fatto di inserire i vincoli
             numero = rimanenti[i]
             parziale.append(numero)
             nuovi_rimanenti = rimanenti[:i] + rimanenti[i+1:]  # salto elemento i
             ricorsione(parziale, nuovi_rimanenti, N)
             parziale.pop()
+
 
 if __name__ == '__main__':
     quadrato_magico(3)
